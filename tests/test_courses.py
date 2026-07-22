c6e5b92d169c4798120fab82fb371464963b42a0
ekaterinazimakina <ekaterina.kanel@gmail.com>
2026-07-22 12:33:32 +0200

    Практикуемся в реализации PageComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -9,10 +9,11 @@ from pages.create_course_page import CreateCoursePage
 @pytest.mark.regression
 def test_empty_courses_list(course_list_page: CoursesListPage):
     course_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+
     course_list_page.navbar.check_visible('username')
     course_list_page.sidebar.check_visible()
-    course_list_page.check_visible_courses_title()
-    course_list_page.check_visible_create_course_button()
+
+    course_list_page.toolbar_view.check_visible()
     course_list_page.check_visible_empty_view()
 
 
@@ -24,13 +25,8 @@ def test_create_course(
 ):
     create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
     create_course_page.check_visible_create_course_title()
-
     create_course_page.check_disabled_create_course_button()
-
-    create_course_page.check_visible_image_preview_empty_view()
-
-    create_course_page.check_visible_image_upload_view()
-
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
     create_course_page.check_visible_create_course_form(
         title='',
         estimated_time='',
@@ -40,14 +36,10 @@ def test_create_course(
     )
 
     create_course_page.check_visible_exercises_title()
-
     create_course_page.check_visible_create_exercise_button()
-
     create_course_page.check_visible_exercises_empty_view()
-
-    create_course_page.upload_preview_image(file='./testdata/files/image.png')
-
-    create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
+    create_course_page.image_upload_widget.upload_preview_image(file='./testdata/files/image.png')
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
 
     create_course_page.fill_create_course_form(
         title="Playwright",
@@ -58,12 +50,9 @@ def test_create_course(
     )
 
     create_course_page.click_create_course_button()
+    course_list_page.toolbar_view.check_visible()
 
-    course_list_page.check_visible_courses_title()
-
-    course_list_page.check_visible_create_course_button()
-
-    course_list_page.check_visible_course_card(
+    course_list_page.course_view.check_visible(
         index=0,
         title='Playwright',
         max_score='100',
