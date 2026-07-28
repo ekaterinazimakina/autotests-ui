8ba2984272f58af27906e0477e466ee26c7a0018
ekaterinazimakina <ekaterina.kanel@gmail.com>
2026-07-28 14:00:52 +0200

    Интеграция Allure в автотесты

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -4,10 +4,24 @@ from pages.courses.courses_list_page import CoursesListPage
 
 from pages.courses.create_course_page import CreateCoursePage
 
+import allure
+
+from tools.allure.tags import AllureTag
+
+from tools.allure.epics import AllureEpics
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
+from allure_commons.types import Severity
 
 @pytest.mark.courses
 @pytest.mark.regression
+@allure.tag(AllureTag.REGRESSION, AllureTag.COURSES)
+@allure.epic(AllureEpics.LMS)
+@allure.feature(AllureFeature.COURSES)
+@allure.story(AllureStory.COURSES)
 class TestCourses:
+    @allure.title('Check displaying of empty courses list')
+    @allure.severity(Severity.NORMAL)
     def test_empty_courses_list(self, course_list_page: CoursesListPage):
         course_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
@@ -17,6 +31,8 @@ class TestCourses:
         course_list_page.toolbar_view.check_visible()
         course_list_page.check_visible_empty_view()
 
+    @allure.title('Create course')
+    @allure.severity(Severity.CRITICAL)
     def test_create_course(
             self,
             create_course_page: CreateCoursePage,
@@ -57,6 +73,8 @@ class TestCourses:
             estimated_time='2 weeks'
         )
 
+    @allure.title('Edit course')
+    @allure.severity(Severity.CRITICAL)
     def test_edit_course(
             self,
             course_list_page: CoursesListPage,
---
+++ tools/allure/epics.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class AllureEpics(str, Enum):
+    LMS = 'LMS system'
+    STUDENT = 'Student system'
+    ADMINISTRATION = 'Administration system'
