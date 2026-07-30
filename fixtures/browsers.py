b2a4faad4e665e0ed1ddfdaeb31e74bef5fbdf1a
ekaterinazimakina <ekaterina.kanel@gmail.com>
2026-07-30 11:15:34 +0200

    Добавление видео в Allure отчет

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,55 +1,35 @@
 import pytest
-from playwright.sync_api import Page, Playwright
+from _pytest.fixtures import SubRequest
+from playwright.sync_api import Playwright, Page
 
 from pages.authentication.registration_page import RegistrationPage
-
-from _pytest.fixtures import SubRequest
-
-import allure
+from tools.playwright.pages import initialize_playwright_page
 
 
 @pytest.fixture
 def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
+    yield from initialize_playwright_page(playwright, test_name=request.node.name)
 
-    yield context.new_page()
 
-    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
-    browser.close()
-
-    allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
-
-@pytest.fixture(scope='session')
-def initialize_browser_state(playwright: Playwright) -> None:
+@pytest.fixture(scope="session")
+def initialize_browser_state(playwright: Playwright):
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
 
     registration_page = RegistrationPage(page=page)
     registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-    registration_page.registration_form.fill(
-        email='user.name@gmail.com',
-        username='username',
-        password='password'
-    )
-    registration_page.registration_button.click()
+    registration_page.registration_form.fill(email='user.name@gmail.com', username='username', password='password')
+    registration_page.click_registration_button()
 
-    context.storage_state(path='browser-state.json')
-
+    context.storage_state(path="browser-state.json")
     browser.close()
 
 
 @pytest.fixture
-def chromium_page_with_state(request: SubRequest, initialize_browser_state, playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state='browser-state.json')
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
-
-    yield context.new_page()
-
-    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
-    browser.close()
-
-    allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
+def chromium_page_with_state(initialize_browser_state, request: SubRequest, playwright: Playwright) -> Page:
+    yield from initialize_playwright_page(
+        playwright,
+        test_name=request.node.name,
+        storage_state="browser-state.json"
+    )
