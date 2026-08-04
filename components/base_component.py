c395543d22e038554a3f496de64a35a41b5cbf24
ekaterinazimakina <ekaterina.kanel@gmail.com>
2026-08-04 11:59:06 +0200

    Логирование взаимодействий со страницей. Практика работы с логированием

--- components/base_component.py
+++ components/base_component.py
@@ -1,9 +1,18 @@
 from playwright.sync_api import Page, expect
 from typing import Pattern
+import allure
+
+from tools.logger import get_logger
+
+logger = get_logger('BASE_COMPONENT')
 
 class BaseComponent:
     def __init__(self, page: Page):
         self.page = page
 
     def check_current_url(self, current_url: Pattern[str]):
-        expect(self.page).to_have_url(current_url)
+        step = f'Checking that current url matches pattern "{current_url.pattern}"'
+
+        with allure.step(step):
+            logger.info(step)
+            expect(self.page).to_have_url(current_url)
--- elements/input.py
+++ elements/input.py
@@ -4,6 +4,9 @@ from playwright.sync_api import expect, Locator
 
 import allure
 
+from tools.logger import get_logger
+
+logger = get_logger('INPUT')
 
 class Input(BaseElement):
     @property
@@ -15,11 +18,17 @@ class Input(BaseElement):
 
 
     def fill(self, value: str, nth: int = 0, **kwargs):
-        with allure.step(f'Fill {self.type_of} to value "{value}"'):
+        step = f'Fill {self.type_of} to value "{value}"'
+
+        with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
+            logger.info(step)
             locator.fill(value)
 
     def check_have_value(self, value: str, nth: int = 0, **kwargs):
-        with allure.step(f'Checking that {self.type_of} "{self.name}" has a value "{value}"'):
+        step = f'Checking that {self.type_of} "{self.name}" has a value "{value}"'
+
+        with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
+            logger.info(step)
             expect(locator).to_have_value(value)
