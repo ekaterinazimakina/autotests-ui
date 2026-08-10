bbfea9d6d23fdba3bd02654296dec94030bb3582
ekaterinazimakina <ekaterina.kanel@gmail.com>
2026-08-10 18:19:13 +0200

    Практикуемся в интеграции покрытия в UI тесты

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import Self
 
-from pydantic import EmailStr, FilePath, HttpUrl, DirectoryPath, Field, BaseModel
+from pydantic import EmailStr, FilePath, HttpUrl, DirectoryPath, BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -23,6 +23,7 @@ class TestData(BaseModel):
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
+        extra='allow',
         env_file=".env",
         env_file_encoding="utf-8",
         env_nested_delimiter=".",
--- elements/input.py
+++ elements/input.py
@@ -1,29 +1,33 @@
-from elements.base_element import BaseElement
-
-from playwright.sync_api import expect, Locator
-
 import allure
+from playwright.sync_api import Locator, expect
+from ui_coverage_tool import ActionType
 
+from elements.base_element import BaseElement
 from tools.logger import get_logger
 
-logger = get_logger('INPUT')
+logger = get_logger("INPUT")
+
 
 class Input(BaseElement):
     @property
     def type_of(self) -> str:
-        return 'input'
+        return "input"
 
     def get_locator(self, nth: int = 0, **kwargs) -> Locator:
         return super().get_locator(nth, **kwargs).locator('input')
 
+    def get_raw_locator(self, nth: int = 0, **kwargs) -> str:
+        return f'{super().get_raw_locator(**kwargs)}//input'
 
     def fill(self, value: str, nth: int = 0, **kwargs):
-        step = f'Fill {self.type_of} to value "{value}"'
+        step = f'Fill {self.type_of} "{self.name}" to value "{value}"'
 
         with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             locator.fill(value)
+
+        self.track_coverage(ActionType.FILL, nth, **kwargs)
 
     def check_have_value(self, value: str, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" has a value "{value}"'
@@ -32,3 +36,5 @@ class Input(BaseElement):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_have_value(value)
+
+        self.track_coverage(ActionType.VALUE, nth, **kwargs)
